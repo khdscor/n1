4f0f9cdf89c8f79c1d0b9f57af91c88eb12b5a12
khdscor <khds_cor@naver.com>
2021-07-13 22:18:27 +0900

    feat: Comment 폴더 및 Domain 구현

---
+++ backend/n1/src/main/java/n1/n1/comment/domain/Comment.java
@@ -0,0 +1,45 @@
+package n1.n1.comment.domain;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import n1.n1.article.domain.Article;
+import n1.n1.user.domain.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="comment_id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="article_id")
+    private Article article;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp createdDate;
+
+}
+
